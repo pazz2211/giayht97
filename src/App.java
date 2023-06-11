92793eb7ccea7f5d264e11ad0f4011ee77862c63
pazz2211 <tunganh993@gmail.com>
2023-06-11 19:28:57 +0700

    test

---
+++ src/App.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //<KDL> variable;
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        input.nextLine();
+        String str = input.nextLine();
+
+        System.out.println("n = " + n);
+        System.out.println("Chuoi vua nhap la: "+str);
+    }
+}
